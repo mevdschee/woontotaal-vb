f2a19ec8f88380d33bcaebc98d7eb3fe4ef7b78b
Maurits van der Schee <maurits@vdschee.nl>
2019-09-07 01:24:17 +0200

    Fix strict warning

--- Program.vb
+++ Program.vb
@@ -7,8 +7,8 @@ Imports Newtonsoft.Json.Linq
 Module Program
 
     Public Sub Main(args As String())
-        Dim Config as Config = New Config("config.json")
-        Dim Api As WoonTotaal.Api = new WoonTotaal.Api(Config.Url, Config.ApiKey, Config.Company, Config.Username, Config.Password)
+        Dim Config as Utils.Config = New Utils.Config("config.json")
+        Dim Api As WoonTotaal.Api = new WoonTotaal.Api(Config.Value("Url"), Config.Value("ApiKey"), Config.Value("Company"), Config.Value("Username"), Config.Value("Password"))
         Dim Materials As List(Of String) = Api.GetListOfMaterials("12")
         For Each s In Materials
             Console.WriteLine(s)
