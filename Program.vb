4abd9e3902adf221855eb9105c059a172f6a11e9
Maurits van der Schee <maurits@vdschee.nl>
2019-09-11 13:16:37 +0200

    Add properties and prices

--- Program.vb
+++ Program.vb
@@ -9,20 +9,20 @@ Module Program
     Public Sub Main(args As String())
         Dim Config as Utils.Config = New Utils.Config("config.json")
         Dim Api As WoonTotaal.Api = new WoonTotaal.Api(Config.Value("Url"), Config.Value("ApiKey"), Config.Value("Company"), Config.Value("Username"), Config.Value("Password"))
-        Dim Materials As List(Of String) = Api.GetListOfMaterials("5230")
-        For Each s In Materials
-            Console.WriteLine(s)
-        Next
-        Console.WriteLine("Press Enter to continue ...")
-        Console.ReadLine()
-        Dim Models As List(Of String) = Api.GetListOfModels()
-        For Each s In Models
-            Console.WriteLine(s)
-        Next
-        Console.WriteLine("Press Enter to continue ...")
-        Console.ReadLine()
+        'Dim Materials As List(Of String) = Api.GetListOfMaterials("5230")
+        'For Each s In Materials
+        '    Console.WriteLine(s)
+        'Next
+        'Console.WriteLine("Press Enter to continue ...")
+        'Console.ReadLine()
+        'Dim Models As List(Of String) = Api.GetListOfModels()
+        'For Each s In Models
+        '    Console.WriteLine(s)
+        'Next
+        'Console.WriteLine("Press Enter to continue ...")
+        'Console.ReadLine()
         Dim Properties As List(Of String) = Api.GetPropertiesForOrder(8114,55406,400,250)
-        For Each s In Models
+        For Each s In Properties
             Console.WriteLine(s)
         Next
         Console.WriteLine("Press Enter to continue ...")
--- WoonTotaal.vb
+++ WoonTotaal.vb
@@ -48,7 +48,7 @@ Module WoonTotaal
                 Dim myMaterialId as JToken = myModelName.SelectToken("$.id")
                 Dim myIsWage as JToken = myModelName.SelectToken("$.isWage")
                 If Not CBool(myIsWage) Then
-                    myModelStrings.Add(Cstr(myDescription) & " / " & Cstr(myMaterialId))
+                    myModelStrings.Add(Cstr(myDescription) & " ~ " & Cstr(myMaterialId))
                 End If
             Next
             Return myModelStrings
@@ -62,7 +62,7 @@ Module WoonTotaal
                 If nextChildren IsNot Nothing AndAlso nextChildren.Count()>0 Then
                     AddToListOfModels(nextChildren,myModelStrings)
                 Else
-                    myModelStrings.Add(Cstr(myDescription) & " / " & Cstr(myModelId))
+                    myModelStrings.Add(Cstr(myDescription) & " ~ " & Cstr(myModelId))
                 End If
             Next
         End Sub
@@ -79,6 +79,42 @@ Module WoonTotaal
             Return myModelStrings            
         End Function
 
+        Private Function GetDataType(DataType as Integer) As String
+            Dim myTypes As Dictionary(Of Integer, String) = New Dictionary(Of Integer, String)()
+            myTypes.Add(1,"label")
+            myTypes.Add(2,"double")
+            myTypes.Add(4,"integer")
+            myTypes.Add(10,"boolean")
+            myTypes.Add(11,"string")
+            Return myTypes(DataType)
+        End Function
+
+        Private Function GetListValues(myProperty as JToken) As Dictionary(Of String, String)
+            Dim myListValues As IEnumerable(Of JToken) = myProperty.SelectTokens("$.listValues[*]")
+            Dim myListValueStrings As Dictionary(Of String, String) = New Dictionary(Of String, String)()
+            If myListValues IsNot Nothing
+                For Each myListValue as JToken In myListValues
+                    Dim myDescription as JToken = myListValue.SelectToken("$.description")
+                    Dim myValue2 as JToken = myListValue.SelectToken("$.value")
+                    myListValueStrings(CStr(myValue2)) = CStr(myDescription)
+                Next
+            End If
+            Return myListValueStrings
+        End Function
+
+        Private Function GetPropertyString(myProperty as JToken) As String
+            Dim myDisplayLabel as JToken = myProperty.SelectToken("$.displayLabel")
+            Dim myValue as JToken = myProperty.SelectToken("$.value")
+            Dim myDisplaySuffix as JToken = myProperty.SelectToken("$.displaySuffix")
+            Dim myDataType as JToken = myProperty.SelectToken("$.dataType")
+            Dim myListValueStrings As Dictionary(Of String, String) = GetListValues(myProperty)
+            Dim myValueString As String = Cstr(myValue)
+            If myListValueStrings.ContainsKey(myValueString) Then
+                myValueString = myListValueStrings(myValueString)
+            End If
+            Dim myListValueString As String = String.Join("`", myListValueStrings.Values)
+            Return Cstr(myDisplayLabel) & "~" & myValueString & "~" & Cstr(myDisplaySuffix) & "~" & GetDataType(CInt(myDataType)) & "~" & myListValueString
+        End Function
 
         Public Function GetPropertiesForOrder(ModelId As Integer, MaterialId As Integer, Width As Integer, Height as Integer) As List(Of String)
             Dim myParams = "?modelId=" & CStr(ModelId) & "&materialId=" & CStr(MaterialId) & "&width=" & CStr(Width) & "&height=" & CStr(Height)
@@ -91,14 +127,35 @@ Module WoonTotaal
             Dim myResp As WebResponse = myReq.GetResponse
             Dim myReader As New System.IO.StreamReader(myResp.GetResponseStream)
             Dim myText As String = myReader.ReadToEnd
+            Dim myParsedText As JObject = JObject.Parse(myText)
+            Dim myProperties As IEnumerable(Of JToken) = myParsedText.SelectTokens("$.polygons[0].categoryLinks[0].categoryLinkMaterial.properties[*]")
+            Dim myPropertyStrings As List(Of String) = New List(Of String)
+            For Each myProperty as JToken In myProperties
+                myPropertyStrings.Add(GetPropertyString(myProperty))
+            Next
 
-'Dim objWriter As New System.IO.StreamWriter( "materials.json" )
-'objWriter.Write( myText )
-'objWriter.Close()
-
-            Dim myChildren As JArray = JArray.Parse(myText)
-            Dim myProperties As List(Of String)  = New List(Of String)
-            Return myProperties            
+            Dim myPriceGroups As IEnumerable(Of JToken) = myParsedText.SelectTokens("$.polygons[0].prices.priceGroups[*]")
+            For Each myPriceGroup as JToken In myPriceGroups
+                Dim myCode as JToken = myPriceGroup.SelectToken("$.code")
+                Dim myUnitName as String = "Arbeid"
+                If CStr(myCode) = "MATERIAL" Then
+                    myUnitName = "Artikel"
+                End If
+                Dim myElements As IEnumerable(Of JToken) = myPriceGroup.SelectTokens("$.elements[*]")
+                For Each myElement as JToken In myElements
+                    Dim myQuantity as JToken = myElement.SelectToken("$.quantity")
+                    Dim myUnit as JToken = myElement.SelectToken("$.unit")
+                    Dim myDescription as JToken = myElement.SelectToken("$.description")
+                    Dim myUnitPrice as JToken = myElement.SelectToken("$.unitPrice")
+                    Dim myTotalPrice as JToken = myElement.SelectToken("$.totalPrice")
+                    Dim myLine As String = Cstr(myQuantity) & "`" & Cstr(myUnit) & "`" & Cstr(myDescription) & "`" & Cstr(myUnitPrice) & "`" & Cstr(myTotalPrice)
+                    myPropertyStrings.Add("Offerteregel~" & myLine & "~" & myUnitName & "~label~")
+                Next
+            Next
+            'Dim objWriter As New System.IO.StreamWriter("properties.json")
+            'objWriter.Write( myText )
+            'objWriter.Close()
+            Return myPropertyStrings
         End Function
 
     End Class
